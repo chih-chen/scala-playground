bbb91d9b93129ff7a69f7384cadfb52e60658d00
chih-chen <chih.chen1029@gmail.com>
2019-06-07 18:50:34 -0300

    add val and def differnce

--- src/main/scala/CallByValueAndCallByName.scala
+++ src/main/scala/CallByValueAndCallByName.scala
@@ -2,12 +2,35 @@ object CallByValueAndCallByName {
 
   def main(args: Array[String]) {
 
-    // cbv
-    def square(x: Int, y: Int) = x * x
-    println(square(1 + 1, 3))
+    def loop: Int = loop
 
-    // cbn
-    def squareCbn(x: => Int, y: => Int) = x * x
-    println(squareCbn(1 + 1, 3))
+
+    // cbv - evaluated to termination, if possible
+    {
+      def square(x: Int, y: Int) = x * x
+
+      println(square(1 + 1, 3))
+    }
+
+    // cbn - not evaluated until reached
+    {
+      def squareCbn(x: => Int, y: => Int) = x * x
+
+      // it works because `loop` is not reached
+      println(squareCbn(1 + 1, loop))
+    }
+
+    // val - right-hand side of val is evaluated at the point of definition itself
+    {
+      // this causes infinite loop, try it on REPL
+      // val byValue = loop
+    }
+
+    // def - evaluated on each use, try it on REPL
+    {
+      def byName = loop
+      // this is ok
+    }
+
   }
 }
